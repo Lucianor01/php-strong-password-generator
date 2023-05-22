fba5a7c7a8864ec8f25337088836665d35d4cdd3
Lucianor01 <lucianorosario02@gmail.com>
2023-05-22 17:47:23 +0200

    Milestone 2

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
 <?php
 
-function generateRandomPassword($length, $characterRepeat, $includeLetters, $includeNumbers, $includeSymbols)
-{
-    $characters = '';
-    if ($includeLetters) $characters = $characters . 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if ($includeNumbers) $characters = $characters . '0123456789';
-    if ($includeSymbols) $characters = $characters . '!@#$%^&*()';
-
-
-    $password = '';
-    $characterCount = strlen($characters);
-
-    for ($i = 0; $i < $length; $i++) {
-        if (!$characterRepeat && $i > 0) {
-            $previousCharacter = $password[$i - 1];
-            $characters = str_replace($previousCharacter, '', $characters);
-            $characterCount = strlen($characters);
-        }
-        $randomIndex = rand(0, $characterCount - 1);
-        $password = $password . $characters[$randomIndex];
-    }
-
-    return $password;
-}
+include __DIR__ . '/include/partials/functions.php';
 
 ?>
 
